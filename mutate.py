7f02006176eceda6dbef11b4994b2a2dec354570
Kevin Reardon <kevr@umich.edu>
2023-02-11 11:07:29 -0500

    Program working

--- mutate.py
+++ mutate.py
@@ -11,25 +11,27 @@ class MutantGen(ast.NodeTransformer):
     # Swap Comparison operators
     def swapComp(self, node):
         for i, comp in enumerate(node.ops):
-            # Change == to !=
-            if comp == ast.Eq:
-                node.ops[i] = ast.NotEq
-            # Change >= to <
-            elif comp == ast.GtE:
-                node.ops[i] = ast.Lt
-            # Change <= to >
-            elif comp == ast.LtE:
-                node.ops[i] = ast.Gt
+            if random.random() < 0.5:
+                # Change == to !=
+                if comp == ast.Eq:
+                    node.ops[i] = ast.NotEq
+                # Change >= to <
+                elif comp == ast.GtE:
+                    node.ops[i] = ast.Lt
+                # Change <= to >
+                elif comp == ast.LtE:
+                    node.ops[i] = ast.Gt
 
     # Swap Binary Operators
     def swapBinOp(self, node):
         for i, op in enumerate(node.op):
-            #swap + with -
-            if op == ast.Add:
-                node.op[i] = ast.Add
-            #swap + with -
-            if op == ast.Sub:
-                node.op[i] = ast.Add
+            if random.random() < 0.5:
+                #swap + with -
+                if op == ast.Add:
+                    node.op[i] = ast.Add
+                #swap + with -
+                if op == ast.Sub:
+                    node.op[i] = ast.Add
     
     #Delete assignment 25% of the time
     def delAssign(self, node):
@@ -50,28 +52,33 @@ def main():
 
     # Build AST from source file
     with open(fileName, "r") as source:
-        sourceTree = ast.parse(source.read)
-    
+        sourceTree = ast.parse(source.read())
+
+    # Save unmodified version of source
+    temp = sourceTree
+    numMut = int(numMut)
     # Initialize mutant generator
     gen = MutantGen()
+    for i in range(numMut):
+        # Reset  SourceTree to unmodified source
+        sourceTree = temp
+        # Apply mutation transformations
+        mutantTree = gen.visit(sourceTree)
+        ast.fix_missing_locations(mutantTree)
+        # Transform mutant AST to source
+        mutantTreeSource = astor.to_source(mutantTree)
 
-    # Apply mutation transformations
-    mutantTree = gen.visit(sourceTree)
-    ast.fix_missing_locations(mutantTree)
-    # Transform mutant AST to source
-    mutantTreeSource = astor.to_source(mutantTree)
+        # Write mutated source to a file
+        with open((str(i) + ".py"), "w") as f:
+            f.write(mutantTreeSource)
 
-    # Write mutated source to a file
-    with open("0.py", "w") as f:
-        f.write(mutantTreeSource)
+        # Make dir if it does not exits
+        #output_dir = "mutDir"
+        #if not os.path.exists(output_dir):  
+        #    os.makedirs(output_dir)
 
-    # Make dir if it does not exits
-    output_dir = "mutDir"
-    if not os.path.exists(output_dir):  
-        os.makedirs(output_dir)
-
-    # Move file to mutant dir
-    shutil.move("0.py", os.path.join(output_dir, "0.py"))
+        # Move file to mutant dir
+       #shutil.move((str(i) + ".py"), os.path.join(output_dir, (str(i) + ".py")))
 
 if __name__ == "__main__":
     main()
