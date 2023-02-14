a0f3a8cf01644276b46c67706b27bac84d2c520a
PeteLast <pclasto@umich.edu>
2023-02-14 16:22:59 -0500

    small edits, mutated docs are all the same

--- mutate.py
+++ mutate.py
@@ -4,12 +4,22 @@ import random
 import astor
 import shutil
 import os
+import time
 
-random.seed(0)
+
+# Get the current time in seconds since start
+current_time = int(time.time())
+#print ("Current time", current_time)
+
+# Set the seed for the random number generator using the current time
+random.seed(current_time)
+
 
 class MutantGen(ast.NodeTransformer):
+    print("test1")
     # Swap Comparison operators
     def swapComp(self, node):
+        print("test2")
         for i, comp in enumerate(node.ops):
             if random.random() < 0.5:
                 # Change == to !=
@@ -24,21 +34,24 @@ class MutantGen(ast.NodeTransformer):
 
     # Swap Binary Operators
     def swapBinOp(self, node):
+        print("test3")
         for i, op in enumerate(node.op):
             if random.random() < 0.5:
                 #swap + with -
                 if op == ast.Add:
-                    node.op[i] = ast.Add
+                    node.op[i] = ast.Sub
                 #swap + with -
                 if op == ast.Sub:
                     node.op[i] = ast.Add
     
     #Delete assignment 25% of the time
     def delAssign(self, node):
-        if random.random() < 0.75:
-            return node
-        else:
-            return None
+        print("test4")
+        if isinstance(node, ast.Assign) and random.random() < 0.25:
+            return ast.Delete(targets=[ast.Name(id=t.id, ctx=ast.Del()) for t in node.targets])
+        return node
+
+
 
 def main():
     # Command line arg check
@@ -48,37 +61,46 @@ def main():
     
     # Get file and # of mutants 
     fileName = sys.argv[1]
-    numMut = sys.argv[2]
+    try:
+        numMut = int(sys.argv[2])
+        if numMut <= 0:
+            raise ValueError("Number of mutants must be positive")
+    except ValueError:
+        print("Invalid number of mutants")
+        return
+
 
     # Build AST from source file
     with open(fileName, "r") as source:
         sourceTree = ast.parse(source.read())
+        
+        #print(ast.dump(sourceTree))
 
     # Save unmodified version of source
     temp = sourceTree
     numMut = int(numMut)
+
     # Initialize mutant generator
     gen = MutantGen()
     for i in range(numMut):
-        # Reset  SourceTree to unmodified source
+        # Reset SourceTree to unmodified source
         sourceTree = temp
+
         # Apply mutation transformations
         mutantTree = gen.visit(sourceTree)
-        ast.fix_missing_locations(mutantTree)
+        #mutantTree = gen.delAssign(gen.swapComp(gen.swapBinOp(sourceTree)))
+        #mutantTree = gen.delAssign(gen.swapBinOp(gen.swapComp(sourceTree)))
+
+
+        mutantTreeFix = ast.fix_missing_locations(mutantTree)
         # Transform mutant AST to source
-        mutantTreeSource = astor.to_source(mutantTree)
+        mutantTreeSource = astor.to_source(mutantTreeFix)
 
         # Write mutated source to a file
         with open((str(i) + ".py"), "w") as f:
             f.write(mutantTreeSource)
 
-        # Make dir if it does not exits
-        #output_dir = "mutDir"
-        #if not os.path.exists(output_dir):  
-        #    os.makedirs(output_dir)
-
-        # Move file to mutant dir
-       #shutil.move((str(i) + ".py"), os.path.join(output_dir, (str(i) + ".py")))
+        print("Mutant " + str(i) + " created")
 
 if __name__ == "__main__":
     main()
